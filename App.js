466d375a6f6369f54e349af9484a3d9eabb2b8ae
Michellegvl <153462758+Michellegvl@users.noreply.github.com>
2024-10-13 20:36:48 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,46 +1,18 @@
-/*import { Text, SafeAreaView, StyleSheet } from 'react-native';
-
-// You can import supported modules from npm
-import { Card } from 'react-native-paper';
-
-// or any files within the Snack
-import AssetExample from './components/AssetExample';
-
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
-*/
-
 
 import React from 'react';
 import {Text, TextInput, View, Image} from 'react-native';
 
-const Cat = () => {
-  return <Text> Hello, I am your cat!</Text>;
+const CatApp = () => {
+  return (
+    <View>
+    <Image
+    source={{
+      uri: 'https://logoeps.com/wp-content/uploads/2013/05/slipknot-eps-vector-logo.png',
+    }}
+    style={{width: 200, height: 200}}
+    />
+    <Text>Hello, I am your cat!</Text>
+    </View>
+  );
 };
-
-export default Cat;
+export default CatApp;
